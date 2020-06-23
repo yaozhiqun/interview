7b9a52fb605c84ad7bca06aa129fb69abe373f03
yaozhiqun <hiyoyee@gmail.com>
2020-06-23 12:41:40 -0700

    6/22/apartments

--- src/main/scala/rocketlawyer/ArrangeApartments.scala
+++ src/main/scala/rocketlawyer/ArrangeApartments.scala
@@ -4,26 +4,45 @@ object ArrangeApartments extends App {
 
   def countArrangements(stories: Int): Int = {
 
-    def rec(n: Int, last: Int = -1, result: Int = 0): Int = {
+//    def rec(n: Int, last: Int = -1, result: Int = 0): Int = {
+//      if (n > stories)
+//          result + 1
+//      else {
+//        last match {
+//          case 0 =>
+//            rec(n + 1, 0, result) + rec(n + 1, 1, result)
+//          case 1 =>
+//            rec(n + 1, 0, result)
+//          case -1 =>
+//            rec(n + 1, 0, result)
+//        }
+//      }
+//    }
+//    rec(1)
+
+    def rec(n: Int, current: List[Int] = Nil, result: List[List[Int]] = Nil): List[List[Int]] = {
       if (n > stories)
-          result + 1
+        current :: result
       else {
-        last match {
-          case 0 =>
-            rec(n + 1, 0, result) + rec(n + 1, 1, result)
-          case 1 =>
-            rec(n + 1, 0, result)
-          case -1 =>
-            rec(n + 1, 0, result)
+        current match {
+          case 0 :: tail =>
+            rec(n + 1, 0 :: current, result) ::: rec(n + 1, 1 :: tail, result)
+          case 1 :: _ =>
+            rec(n + 1, 0 :: current, result)
+          case Nil =>
+            rec(n + 1, List(0), result)
+
         }
       }
     }
-    rec(1)
+    val arrangements = rec(1)
+    println(arrangements)
+    arrangements.size
   }
 
   println(countArrangements(1))
   println(countArrangements(2))
   println(countArrangements(3))
   println(countArrangements(4))
-  println(countArrangements(20))
+//  println(countArrangements(20))
 }
